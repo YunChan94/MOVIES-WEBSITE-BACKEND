b3d948e25e5fbccfbc552aef5d6e8402b0454cf9
YunChan94 <juleefang94@gmail.com>
2023-02-10 16:40:00 +0700

    ket noi voi fe

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const moviesRoutes = require("./routes/movie");
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/movies", moviesRoutes);
